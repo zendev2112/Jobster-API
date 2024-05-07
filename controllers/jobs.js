55cb1b88730a6474503328c6b2df805b6fb3abbb
zen <zenon.deviagge@gmail.com>
2024-05-07 15:47:50 -0300

    Add `showStats` controller. Export `showStats` controller. Setup `showStats` controller in the `stats` route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -113,10 +113,17 @@ const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).send();
 };
 
+const showStats = async (req, res) => {
+  res
+    .status(StatusCodes.OK)
+    .json({ defaultStats: {}, monthlyApplications: [] });
+};
+
 module.exports = {
   createJob,
   deleteJob,
   getAllJobs,
   updateJob,
   getJob,
+  showStats,
 };
